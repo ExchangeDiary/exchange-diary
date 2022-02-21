c69e066cd85444cd595a905f08eeca35be9b8852
minkj1992 <minkj1992@gmail.com>
2022-02-21 19:03:10 +0900

    feat: Added base model scheam

---
+++ exchange-diary/infrastructure/persistence/base.go
@@ -0,0 +1,27 @@
+package persistence
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID      	uint   `gorm:"primaryKey"`
+	CreatedAt 		time.Time
+	UpdatedAt 		time.Time
+}
+
+func (base *BaseModel) BeforeCreate(tx *gorm.DB) error {
+	currentDateTime, err := currentDateTime()
+	if err != nil {
+	  return err
+	}
+	tx.Statement.SetColumn("CreatedAt", currentDateTime)
+	tx.Statement.SetColumn("UpdatedAt", currentDateTime)
+	return nil
+}
+
+func currentDateTime() (time.Time, error) {
+	return time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+}
